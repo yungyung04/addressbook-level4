705f7247d065a286fca7ff1ffb4b57868ef51507
a-shakra <shakra.anas@gmail.com>
2018-04-15 14:09:41 +0800

    Fixed some checkstyle errors

--- src/test/java/seedu/address/storage/XmlSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/XmlSerializableAddressBookTest.java
@@ -1,7 +1,5 @@
 package seedu.address.storage;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 
 import org.junit.Rule;
@@ -11,9 +9,6 @@ import org.junit.rules.ExpectedException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.XmlUtil;
-import seedu.address.model.AddressBook;
-import seedu.address.testutil.TypicalTasks;
-import seedu.address.testutil.typicaladdressbook.TypicalAddressBookCompiler;
 
 public class XmlSerializableAddressBookTest {
 
