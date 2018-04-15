7a536f31d03e0848aad41467f330bdcf0f44621d
yungyung04 <ekabuyung@gmail.com>
2018-04-15 19:55:48 +0800

    remove comment

--- src/main/java/seedu/address/model/personal/PersonalTask.java
+++ src/main/java/seedu/address/model/personal/PersonalTask.java
@@ -119,43 +119,12 @@ public class PersonalTask implements Task {
     }
 
     /**
-     * Returns true if the two tasks are equal. Needs to be updated to reflect the name parameter
-     */
-
-
-    /**
      * Returns true if the tuition task contains a non-empty description.
      */
     private boolean hasDescription() {
         return !description.equals(NULL_STRING);
     }
 
-
-    /**
-     * this fixes the valid args test, but has conflict with Task card
-     * ^ I, a-shakra, didn't write this but I kept it because I wasn't sure what is meant by it
-     * Remove if all is clear but notify me before please
-     */
-    //@@author a-shakra
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        if (!(other instanceof Task)) {
-            return false;
-        }
-
-        Task otherTask = (Task) other;
-        return otherTask.getDescription().equals(this.getDescription())
-                && otherTask.getDuration().equals(this.getDuration())
-                && otherTask.getTaskDateTime().toString().equals(this.getTaskDateTime().toString());
-    }
-
-    /**
-     * this fixes the valid args test, but has conflict with Task card
-     * This is the old equals. TO be determined which to keep
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
@@ -164,6 +133,5 @@ public class PersonalTask implements Task {
                 && duration.equals(((PersonalTask) other).duration)
                 && description.equals(((PersonalTask) other).description));
     }
-    */
 
 }
